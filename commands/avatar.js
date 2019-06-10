0bddfab54a74da0d072073889725ce63866c72c6
Richard Thompson <arcticuprising1471@gmail.com>
2019-06-10 20:02:06 +0100

    avatar

--- commands/avatar.js
+++ commands/avatar.js
@@ -8,8 +8,8 @@ module.exports = {
     
         const avatarList = message.mentions.users.map(user => {
             return `${user.username}'s avatar: <${user.displayAvatarURL}>`;
+        
         });
-    
         // send the entire array of strings as a message
         // by default, discord.js will `.join()` the array with `\n`
         message.channel.send(avatarList);
