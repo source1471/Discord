28e132d1861ffe7f52cd7af2309938eb6d362d52
Richard Thompson <arcticuprising1471@gmail.com>
2019-06-10 19:40:18 +0100

    added comand handler

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 // require the discord.js module
 const Discord = require('discord.js');
 // require the prefix and token
@@ -5,19 +7,24 @@ const {prefix, token } = require('./config.json');
 
 // create a new Discord client
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
 client.once('ready', () => {
 	console.log('Ready!');
 });
-// checks if message contains ! or if it was sent by another bot
-if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-const args = message.content.slice(prefix.length).split(/ +/);
-const command = args.shift().toLowerCase();
-// logs messages
 client.on('message', message => {
+	// checks if message contains ! or if it was sent by another botn
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/);
